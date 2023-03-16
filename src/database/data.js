87c4fae6466030b05d259d86d116e35d6506b809
AyushPanday1 <ayush7426883261@gmail.com>
2023-03-16 10:44:31 +0530

    Client side

---
+++ src/database/data.js
@@ -0,0 +1,51 @@
+export default [
+    {
+        id:1,
+        question:"Javascript is an ________ language",
+        options: [
+            'Object-oriented',
+            'Object-based',
+            'Procedural'
+        ]
+    },
+
+    {
+        id:2,
+        question:"Javascript is an ________ language",
+        options: [
+            'Object-oriented',
+            'Object-based',
+            'Procedural'
+        ]
+    },
+
+    {
+        id:3,
+        question:"Javascript is an ________ language",
+        options: [
+            'Object-oriented',
+            'Object-based',
+            'Procedural'
+        ]
+    },
+
+    {
+        id:4,
+        question:"Javascript is an ________ language",
+        options: [
+            'Object-oriented',
+            'Object-based',
+            'Procedural'
+        ]
+    },
+
+    {
+        id:5,
+        question:"Javascript is an ________ language",
+        options: [
+            'Object-oriented',
+            'Object-based',
+            'Procedural'
+        ]
+    }
+]
